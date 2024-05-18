3b21dbea8c453620e06e9a85221f8aeaa7c33772
SteveNoyes <stevenmnoyes.af@gmail.com>
2024-05-18 13:59:28 -0400

    user input number of rounds

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import random
 
-
 def main():
 
-  pass
+  print("Welcome to the High-Low Game")
+  print("--------------------------------")
+  print("--------------------------------")
+
+  num_rounds = input("Please select how many rounds you would like to play. ")
+  print(num_rounds)
+
+  
 
 if __name__ == "__main__":
   main()
