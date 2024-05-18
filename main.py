0f3c51e18b369610573511780238cb88e8cfc9b2
SteveNoyes <stevenmnoyes.af@gmail.com>
2024-05-18 14:05:10 -0400

    testing

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ def main():
 
   print("Welcome to the High-Low Game")
   print("--------------------------------")
-  print("--------------------------------")
 
-  num_rounds = input("Please select how many rounds you would like to play. ")
-  print(num_rounds)
+  num_rounds = int(input("Please select how many rounds you would like to play. "))
 
-  
+  for i in range(num_rounds):
+    # generate random numbers for player and computer, each round
+    computer_random_number = random.randint(1, 100)
+    player_random_number = random.randint(1, 100)
+
+    print("The computer's number is", computer_random_number)
+    print("Your number is", player_random_number)
+
 
 if __name__ == "__main__":
   main()
